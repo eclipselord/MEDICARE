333be2479ea60c13355006dc1892e96b2a2f63af
eclipselord <arunsuparnaghosh@gmail.com>
2024-12-02 20:19:50 +0530

    add file sticky

---
+++ sticky nav pra/assets/js/script.js
@@ -0,0 +1,39 @@
+ window.addEventListener("scroll", function(){
+     let header = document.querySelector("header");
+   header.classList.toggle("sticky", window.scrollY );
+ });
+
+// sticky nav************************************************
+
+
+ let calcScrollValue = () => {
+	let scrollProgress = document.getElementById
+	("progress");
+	let progressValue = document.getElementById
+	("progress-value");
+	let pos = document.documentElement.scrollTop;
+	let calcHeight =
+		document.documentElement.scrollHeight  -
+		document.documentElement.clientHeight;
+
+	let ScrollValue = Math.round((pos * 100) / calcHeight)
+	;
+
+	if(pos > 100) {
+		scrollProgress.style.display = "grid";
+
+	} else {
+		scrollProgress.style.display = "none";
+	}
+
+	scrollProgress.addEventListener("click", () => {
+		document.documentElement.scrollTop = 0;
+	});
+
+	
+};
+
+
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
